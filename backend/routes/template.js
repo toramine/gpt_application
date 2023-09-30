060ea0d756a9dc625d0ee0eea3bc049fafb56e1b
TAIGA <>
2023-09-30 09:32:03 +0900

    backend routesディレクトリの作成 backend server.jsの作成と記述

---
+++ backend/routes/template.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// GET:template
+
+// POST:template
+
+// PUT:template
+
+// DELETE:template
+
+// GET one:template
+
+module.exports = router;
