253509d4a291df2ae71e013bb2b361986981c33a
TAIGA <>
2023-10-13 11:14:53 +0900

    _app.jsの作成 TopbarComponentの作成

---
+++ frontend/components/Topbar.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+import styles from "./Topbar.module.css";
+
+export default function TopBar({ pageTitle }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <Link href="/">
+          <div className={styles.text}>トップページへ</div>
+        </Link>
+        <h1 className={styles.text}>{pageTitle}</h1>
+        <div className={styles.text}>詳細</div>
+      </div>
+    </div>
+  );
+}
