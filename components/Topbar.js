fb353b9820bcb2d3b43a12e4bb3ac73f0cb78d5f
TAIGA <>
2023-10-19 10:47:48 +0900

    トップバーコンポーネントをページに追加 実装内容をコメントアウトで記述

--- components/Topbar.js
+++ components/Topbar.js
@@ -4,13 +4,15 @@ import styles from "./Topbar.module.css";
 
 export default function TopBar({ pageTitle }) {
   return (
-    <div className={styles.container}>
-      <div className={styles.wrapper}>
-        <Link href="/">
-          <div className={styles.text}>トップページへ</div>
-        </Link>
-        <h1 className={styles.text}>{pageTitle}</h1>
-        <div className={styles.text}>詳細</div>
+    <div className={styles.topbar}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <Link href="/">
+            <div className={styles.text}>トップページへ</div>
+          </Link>
+          <h1 className={styles.text}>{pageTitle}</h1>
+          <div className={styles.text}>詳細</div>
+        </div>
       </div>
     </div>
   );
--- pages/gpt.js
+++ pages/gpt.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TemplateFalse from "../components/TemplateFalse";
 import TemplateTrue from "../components/TemplateTrue";
+import Topbar from "../components/Topbar";
 import styles from "../styles/Gpt.module.css";
 
 const Gpt = () => {
@@ -25,47 +26,59 @@ const Gpt = () => {
   const handleSubmit = () => {
     const responseText = `選択: ${selectedButton}, 質問: ${question}`;
     setResponse(responseText);
+    // バリデーションを設定する
+    // 質問を送信する
+    // レスポンスをupdateQueryResultに格納する
+    // レスポンスを待つ間は実行中を表示
   };
 
   return (
-    <div className={styles.container}>
-      <h1>質問フォーム</h1>
-      <div className={styles.buttons}>
-        <button
-          className={`${styles.button} ${
-            selectedButton === "gpt-3.5-turbo" ? styles.selected : ""
-          }`}
-          onClick={() => handleButtonClick("gpt-3.5-turbo")}
-        >
-          gpt-3.5-turbo
-        </button>
-        <button
-          className={`${styles.button} ${
-            selectedButton === "gpt-4" ? styles.selected : ""
-          }`}
-          onClick={() => handleButtonClick("gpt-4")}
-        >
-          gpt-4
-        </button>
+    <div>
+      <div className={styles.topbar}>
+        <Topbar pageTitle="GPT API" />
       </div>
-      <p>templateFlag: {templateFlag ? "true" : "false"}</p>
-      <button onClick={toggleTemplateFlag}>Toggle templateFlag</button>
-      {templateFlag ? (
-        <TemplateTrue />
-      ) : (
-        <TemplateFalse templateFlag={templateFlag} setQuestion={setQuestion} />
-      )}
-      <button className={styles["submit-button"]} onClick={handleSubmit}>
-        質問を送信
-      </button>
-      <div className={styles.response}>
-        <p>{response}</p>
-      </div>
-      <div>
-        <p>queryResult: {queryResult}</p>
-        <button onClick={() => updateQueryResult("New Query Result")}>
-          Update queryResult
+      <div className={styles.container}>
+        <h1>質問フォーム</h1>
+        <div className={styles.buttons}>
+          <button
+            className={`${styles.button} ${
+              selectedButton === "gpt-3.5-turbo" ? styles.selected : ""
+            }`}
+            onClick={() => handleButtonClick("gpt-3.5-turbo")}
+          >
+            gpt-3.5-turbo
+          </button>
+          <button
+            className={`${styles.button} ${
+              selectedButton === "gpt-4" ? styles.selected : ""
+            }`}
+            onClick={() => handleButtonClick("gpt-4")}
+          >
+            gpt-4
+          </button>
+        </div>
+        <p>templateFlag: {templateFlag ? "true" : "false"}</p>
+        <button onClick={toggleTemplateFlag}>Toggle templateFlag</button>
+        {templateFlag ? (
+          <TemplateTrue />
+        ) : (
+          <TemplateFalse
+            templateFlag={templateFlag}
+            setQuestion={setQuestion}
+          />
+        )}
+        <button className={styles["submit-button"]} onClick={handleSubmit}>
+          質問を送信
         </button>
+        <div className={styles.response}>
+          <p>{response}</p>
+        </div>
+        <div>
+          <p>queryResult: {queryResult}</p>
+          <button onClick={() => updateQueryResult("New Query Result")}>
+            Update queryResult
+          </button>
+        </div>
       </div>
     </div>
   );
